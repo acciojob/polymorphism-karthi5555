3b097e3551785f36e7b76b27e5957a65b5d60511
Karthik M <karthikm7483@gmail.com>
2023-07-11 08:09:25 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
 
-    public static class product{
+    public static class Product{
 
         public int product(int x, int y){
             return 0;
@@ -21,7 +21,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        product p = new product();
+        Product p = new Product();
 
         //calling the methods
         p.product(1,2);
